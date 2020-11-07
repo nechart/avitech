9897945891fa2fe847d6257eb13047d21676bbf3
Артем <nechart80@gmail.com>
2020-11-07 13:37:04 +0300

    krest

--- package/avi/dangerlab/levels.py
+++ package/avi/dangerlab/levels.py
@@ -56,6 +56,13 @@ level_search_grid_map =\
  [1,0,1,0,1],
  [1,1,1,3,3]] 
 
+level_search_krest_map =\
+[[2,1,0,1,3],
+ [1,1,1,1,1],
+ [1,0,0,1,0],
+ [1,1,0,1,1],
+ [1,3,1,3,1]] 
+
 level_search_empty_enemy_map =\
 [[2,1,1,1,1],
  [1,1,3,1,1],
@@ -67,7 +74,10 @@ levels_task = { 'search_border':[level_search_border_map, 0],
                 'search_empty':[level_search_empty_map, 0],
                 'search_diag':[level_search_diag_map, 0],
                 'search_grid':[level_search_grid_map, 0],
-                'search_empty_enemy':[level_search_empty_enemy_map, 3],
+                'search_krest':[level_search_krest_map, 0],
+                'search_empty_enemy1':[level_search_empty_enemy_map, 1],
+                'search_empty_enemy2':[level_search_empty_enemy_map, 2],
+                'search_empty_enemy3':[level_search_empty_enemy_map, 3],
                 'level1':[level1_map, 1],
                 'level2':[level2_map, 1],
                 'level3':[level3_map, 3],
